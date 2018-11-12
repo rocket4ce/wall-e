a1625c81c59762cb0c8def6e36aa8f3253f91aa5
Rocket <rocket4ce@gmail.com>
2018-11-12 16:19:16 -0300

    Create JobActive SendNotificationJob

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comments_params)
     respond_to do |format|
       if @comment.save
-        @comment.send_notification
+        SendNotificationJob.perform_later @comment
         format.html { redirect_to root_path, notice: 'Haz creado un commentario' }
       else
         format.html { redirect_to root_path, error: 'No se ha creado el mensaje' }
---
+++ app/jobs/send_notification_job.rb
@@ -0,0 +1,7 @@
+class SendNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(comment)
+    comment.send_notification
+  end
+end
